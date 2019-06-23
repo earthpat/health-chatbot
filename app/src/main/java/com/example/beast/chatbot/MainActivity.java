3f906aff53c82be9a8e00bc9187367d703d4fa1f
Collegeboi67 <jonag01@comcast.net>
2019-06-23 15:11:43 -0400

    Initial commit

---
+++ app/src/main/java/com/example/beast/chatbot/MainActivity.java
@@ -0,0 +1,321 @@
+package com.example.beast.chatbot;
+
+import android.*;
+import android.Manifest;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+/*
+Firebase realtime database and backend as a service.
+The service provides application developers an API that allows application
+data to be synchronized across clients and stored on Firebase's cloud.
+The company provides client libraries that enable integration with Android,
+iOS, JavaScript, Java, Objective-C, Swift and Node.js applications.
+Developers using the realtime database can secure their data
+by using the company's server-side-enforced security rules.
+ */
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+/*
+Dialogflow which was formerly Api.ai is a Google development suite for human–computer
+interaction technologies based on machine learning and natural language conversations.
+This technology performs tasks and answers users' questions in a natural language.
+Speaktoit has also created a natural language processing engine that incorporates conversation
+context like dialogue history, location and user preferences and also allows
+the addition of voice interfaces to apps based on Android,
+iOS, HTML5, and Cordova. The SDK's contain voice recognition, natural language
+understanding, and text-to-speech. api.ai offers a web interface to build and test
+conversation scenarios.
+Dialogflor was initially known as API.AI before Google bought the company in September 2016;
+Voice and conversational interfaces created with Dialogflow works with a
+wide range of devices including phones, wearables, cars, speakers and
+other smart devices. It supports 14+ languages including Brazilian
+Portuguese, Chinese, English, etc...
+ */
+
+import ai.api.AIDataService;
+import ai.api.AIListener;
+import ai.api.AIServiceException;
+import ai.api.android.AIConfiguration;
+import ai.api.android.AIService;
+import ai.api.model.AIError;
+import ai.api.model.AIRequest;
+import ai.api.model.AIResponse;
+import ai.api.model.Result;
+import com.google.gson.JsonElement;
+import java.util.Map;
+
+
+public class MainActivity extends AppCompatActivity implements AIListener{
+
+    // RecyclerView widget is a more advanced and flexible version of ListView;
+    // s a container for displaying large data sets that can be scrolled very
+    // efficiently by maintaining a limited number of views. You can use
+    // the RecyclerView widget when you have data collections whose elements
+    // changes at runtime based on user action or network events. Direct horizontal
+    // scrolling is not supported in ListView. Whereas in RecyclerView you can easily
+    // implement horizontal and vertical scrolling. ListView supports only
+    // simple linear list view with vertical scrolling. Whereas RecyclerView
+    // supports three types of lists using RecyclerView.LayoutManager
+    // StaggeredGridLayoutManager GridLayoutManager LinearLayoutManager
+    //ListView gives you an option to add divider using dividerHeight parameter
+    // whereas RecyclerView enable you to customize divider (spacing) between
+    // two elements using RecyclerView.ItemDecoration class.
+    RecyclerView recyclerView;
+    EditText editText;
+    // The Relative Layout is very flexible layout used in android for
+    // custom layout designing. It gives us the flexibility to position our
+    // component/view based on the relative or sibling component’s position.
+    // In Relative Layout, you can use “above, below, left and right” to
+    // arrange the component’s position in relation to other component.
+    RelativeLayout addBtn;
+    // A  DatabaseReference is a Firebase reference representing a particular location in your Database
+    // and can be used for reading or writing data to that Database location.
+    //This class is the starting point for all Database operations.
+    // After you've initialized it with a URL, you can use it to read data,
+    // write data, and to create new DatabaseReferences.
+    DatabaseReference ref;
+    // FirebaseRecyclerAdapter is a generic way of backing an RecyclerView with
+    // a Firebase location. It handles all of the child events at the given
+    // Firebase location. It marshals received data into the given class type.
+    FirebaseRecyclerAdapter<ChatMessage,chat_rec> adapter;
+    Boolean flagFab = true;
+
+    private AIService aiService;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO},1);
+
+
+        recyclerView = (RecyclerView)findViewById(R.id.recyclerView);
+        editText = (EditText)findViewById(R.id.editText);
+        addBtn = (RelativeLayout)findViewById(R.id.addBtn);
+
+        recyclerView.setHasFixedSize(true);
+        final LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setStackFromEnd(true);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        ref = FirebaseDatabase.getInstance().getReference();
+        ref.keepSynced(true);
+
+        final AIConfiguration config = new AIConfiguration("fecdc26f873e4b569ea09feef9262767",
+                AIConfiguration.SupportedLanguages.English,
+                AIConfiguration.RecognitionEngine.System);
+
+        aiService = AIService.getService(this, config);
+        aiService.setListener(this);
+
+        final AIDataService aiDataService = new AIDataService(config);
+
+        final AIRequest aiRequest = new AIRequest();
+
+
+
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String message = editText.getText().toString().trim();
+
+                if (!message.equals("")) {
+
+                    ChatMessage chatMessage = new ChatMessage(message, "user");
+                    ref.child("chat").push().setValue(chatMessage);
+
+                    aiRequest.setQuery(message);
+                    new AsyncTask<AIRequest,Void,AIResponse>(){
+
+                        @Override
+                        protected AIResponse doInBackground(AIRequest... aiRequests) {
+                            final AIRequest request = aiRequests[0];
+                            try {
+                                final AIResponse response = aiDataService.request(aiRequest);
+                                return response;
+                            } catch (AIServiceException e) {
+                            }
+                            return null;
+                        }
+                        @Override
+                        protected void onPostExecute(AIResponse response) {
+                            if (response != null) {
+
+                                Result result = response.getResult();
+                                String reply = result.getFulfillment().getSpeech();
+                                ChatMessage chatMessage = new ChatMessage(reply, "bot");
+                                ref.child("chat").push().setValue(chatMessage);
+                            }
+                        }
+                    }.execute(aiRequest);
+                }
+                else {
+                    aiService.startListening();
+                }
+
+                editText.setText("");
+
+            }
+        });
+
+
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                ImageView fab_img = (ImageView)findViewById(R.id.fab_img);
+                Bitmap img = BitmapFactory.decodeResource(getResources(),R.drawable.ic_send_white_24dp);
+                Bitmap img1 = BitmapFactory.decodeResource(getResources(),R.drawable.ic_mic_white_24dp);
+
+
+                if (s.toString().trim().length()!=0 && flagFab){
+                    ImageViewAnimatedChange(MainActivity.this,fab_img,img);
+                    flagFab=false;
+
+                }
+                else if (s.toString().trim().length()==0){
+                    ImageViewAnimatedChange(MainActivity.this,fab_img,img1);
+                    flagFab=true;
+
+                }
+
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        adapter = new FirebaseRecyclerAdapter<ChatMessage, chat_rec>(ChatMessage.class,R.layout.msglist,chat_rec.class,ref.child("chat")) {
+            @Override
+            protected void populateViewHolder(chat_rec viewHolder, ChatMessage model, int position) {
+
+                if (model.getMsgUser().equals("user")) {
+
+
+                    viewHolder.rightText.setText(model.getMsgText());
+
+                    viewHolder.rightText.setVisibility(View.VISIBLE);
+                    viewHolder.leftText.setVisibility(View.GONE);
+                }
+                else {
+                    viewHolder.leftText.setText(model.getMsgText());
+
+                    viewHolder.rightText.setVisibility(View.GONE);
+                    viewHolder.leftText.setVisibility(View.VISIBLE);
+                }
+            }
+        };
+
+        adapter.registerAdapterDataObserver(new RecyclerView.AdapterDataObserver() {
+            @Override
+            public void onItemRangeInserted(int positionStart, int itemCount) {
+                super.onItemRangeInserted(positionStart, itemCount);
+
+                int msgCount = adapter.getItemCount();
+                int lastVisiblePosition = linearLayoutManager.findLastCompletelyVisibleItemPosition();
+
+                if (lastVisiblePosition == -1 ||
+                        (positionStart >= (msgCount - 1) &&
+                                lastVisiblePosition == (positionStart - 1))) {
+                    recyclerView.scrollToPosition(positionStart);
+
+                }
+
+            }
+        });
+
+        recyclerView.setAdapter(adapter);
+
+
+    }
+    public void ImageViewAnimatedChange(Context c, final ImageView v, final Bitmap new_image) {
+        final Animation anim_out = AnimationUtils.loadAnimation(c, R.anim.zoom_out);
+        final Animation anim_in  = AnimationUtils.loadAnimation(c, R.anim.zoom_in);
+        anim_out.setAnimationListener(new Animation.AnimationListener()
+        {
+            @Override public void onAnimationStart(Animation animation) {}
+            @Override public void onAnimationRepeat(Animation animation) {}
+            @Override public void onAnimationEnd(Animation animation)
+            {
+                v.setImageBitmap(new_image);
+                anim_in.setAnimationListener(new Animation.AnimationListener() {
+                    @Override public void onAnimationStart(Animation animation) {}
+                    @Override public void onAnimationRepeat(Animation animation) {}
+                    @Override public void onAnimationEnd(Animation animation) {}
+                });
+                v.startAnimation(anim_in);
+            }
+        });
+        v.startAnimation(anim_out);
+    }
+
+    @Override
+    public void onResult(ai.api.model.AIResponse response) {
+
+
+        Result result = response.getResult();
+
+        String message = result.getResolvedQuery();
+        ChatMessage chatMessage0 = new ChatMessage(message, "user");
+        ref.child("chat").push().setValue(chatMessage0);
+
+
+        String reply = result.getFulfillment().getSpeech();
+        ChatMessage chatMessage = new ChatMessage(reply, "bot");
+        ref.child("chat").push().setValue(chatMessage);
+
+
+    }
+
+    @Override
+    public void onError(ai.api.model.AIError error) {
+
+    }
+
+    @Override
+    public void onAudioLevel(float level) {
+
+    }
+
+    @Override
+    public void onListeningStarted() {
+
+    }
+
+    @Override
+    public void onListeningCanceled() {
+
+    }
+
+    @Override
+    public void onListeningFinished() {
+
+    }
+}
